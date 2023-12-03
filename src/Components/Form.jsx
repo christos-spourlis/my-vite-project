1aef25f260caac7ca288e36155bef43facd6682f
christos-spourlis <148483515+christos-spourlis@users.noreply.github.com>
2023-12-03 18:25:05 +0200

    commit test3

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,29 +6,76 @@ export default function Validation() {
   const [email, setEmail] = useState("");
   const [errors, setErrors] = useState({});
   const [isSubmit, setSUbmit] = useState(false);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    setErrors(validate(name, lastName, email));
+    setSUbmit(true);
+  }
+  const resetForm = () => {
+    setName("");
+    setLastName("");
+    setEmail("");
+    setErrors({});
+    setSUbmit(false);
+  };
+  useEffect(() => {
+    console.log(errors);
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log(name, lastName, email);
+      resetForm();
+    }
+  }, [errors, isSubmit, name, lastName, email]);
+  function validate(name, lastName, email) {
+    const errors = {};
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!name) {
+      errors.name = "this field is required!";
+    }
+    if (!email) {
+      errors.email = "this field is required!";
+    }
+    if (!lastName) {
+      errors.lastName = "this field is required!";
+    }
+    return errors;
+  }
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Student Form</h1>
         <div className="output">
-          <pre></pre>
-          <pre></pre>
-          <pre></pre>
+          <pre>First Name: {name}</pre>
+          <pre>Last Name: {lastName}</pre>
+          <pre>Email:{email}</pre>
         </div>
         <div className="field">
-          <p></p>
+          <p>{errors.name}</p>
           <label>First Name</label>
-          <input type="text"></input>
+          <input type="text" onChange={handleNameChange} value={name}></input>
         </div>
         <div className="field">
-          <p></p>
+          <p>{errors.lastName}</p>
           <label>Last Name</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            onChange={handleLastNameChange}
+            value={lastName}
+          ></input>
         </div>
         <div className="field">
-          <p></p>
+          <p>{errors.email}</p>
           <label>Email</label>
-          <input type="text"></input>
+          <input type="text" onChange={handleEmailChange} value={email}></input>
         </div>
         <button>Submit</button>
       </form>
